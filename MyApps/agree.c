6ebea6cd5a7f0774ce4ee41719222ae17674c1f6
Essilfie <bquansah007@gmail.com>
2022-04-26 16:42:15 +0000

    Hello World!

---
+++ MyApps/agree.c
@@ -0,0 +1,22 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // Prompt user to agree
+    char c = get_char("Do you agree? ");
+
+    // Check if user agreed
+    if (c == 'y' || c == 'Y')
+    {
+        printf("Agreed.\n");
+    }
+    else if (c == 'n' || c == 'N')
+    {
+        printf("Did not agree\n");
+    }
+    else 
+    {
+        printf("Not an option\n");
+    }
+}

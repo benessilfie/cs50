3e733bdb112d1d9d8864dfb99dca66c6d186f735
Essilfie <bquansah007@gmail.com>
2022-04-27 00:09:55 +0000

    update calculator

--- MyApps/calculator.c
+++ MyApps/calculator.c
@@ -3,7 +3,15 @@
 
 int main(void)
 {
+    // Prompt user for X
     int x = get_int("x: ");
+
+    // Prompt user for Y
     int y = get_int("y: ");
-    printf("%i\n", x + y);
+
+    // Divide x by y
+    float z = (float) x / (float) y;
+
+    // Print answer
+    printf("%.2f\n", z);
 }

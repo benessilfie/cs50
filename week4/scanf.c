39bc1a8ba6f973f1c4fdc4d53737fa63a903080e
Essilfie <bquansah007@gmail.com>
2022-05-01 13:35:12 +0000

    pointers with scanf.c

---
+++ week4/scanf.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    // Using scanf with integers
+    // int x;
+
+    // printf("x: ");
+    // scanf("%i", &x);
+    // printf("x: %i\n", x);
+
+    // Using scanf with strings
+    char *s;
+
+    printf("s: ");
+    scanf("%s", s);
+    printf("s: %s\n", s);
+}

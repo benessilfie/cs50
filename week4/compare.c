2d1fd049a62599648a088769a34dbc94d317b00d
Essilfie <bquansah007@gmail.com>
2022-05-01 10:23:52 +0000

    more pointers with compare.c

--- week4/compare.c
+++ week4/compare.c
@@ -3,15 +3,10 @@
 
 int main(void)
 {
-    int i = get_int("i: ");
-    int j = get_int("j: ");
+    char *s = get_string("s: ");
+    char *t = get_string("t: ");
 
-    if (i == j)
-    {
-        printf("Same\n");
-    }
-    else
-    {
-        printf("Different\n");
-    }
+    printf("%p\n", s);
+    printf("%p\n", &s[0]); // This is the same as the code on line 09
+    printf("%p\n", t);
 }

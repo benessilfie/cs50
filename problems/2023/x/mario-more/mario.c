fb961c4f452cd97fa32db0e3ff33efa6f1b88fed
Essilfie <bquansah007@gmail.com>
2023-07-11 17:13:51 +0000

    Add pset 1: mario

---
+++ problems/2023/x/mario-more/mario.c
@@ -0,0 +1,25 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void) {
+    int height;
+
+    do {
+        height = get_int("Height: ");
+    } while (height < 1 || height > 8);
+
+    for (int i = 0; i < height; i++) {
+        for (int j = i; j < height - 1; j++) {
+            printf(" ");
+        }
+
+        printf("%.*s", i + 1, "########");
+        printf("  ");
+
+        for (int k = 0; k < i + 1; k++) {
+            printf("#");
+        }
+
+        printf("\n");
+    }
+}
